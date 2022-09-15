36607cf3080920859c6328b1401c8c76a2990ba7
coskunccc <coskunarslan@yahoo.com>
2022-09-15 13:03:09 +0300

    Selenium project1 commit DAY9 Faker TestBase

---
+++ src/test/java/com/cydeo/Tests/Day09_JavaFaker_testbase_driver/JavaFaker.java
@@ -0,0 +1,21 @@
+package com.cydeo.Tests.Day09_JavaFaker_testbase_driver;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFaker {
+    @Test
+            public void test1(){
+        Faker faker=new Faker();
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.address().fullAddress() = " + faker.address().fullAddress());
+        System.out.println("faker.numerify(\"###-###-###\") = " + faker.numerify("###-###-###"));
+        System.out.println("faker.letterify(\"???##\") = " + faker.letterify("??????"));
+        System.out.println("faker.bothify(\"????##???###\") = " + faker.bothify("????##???###"));
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard().replaceAll("-"," "));
+        System.out.println("faker.lorem().characters() = " + faker.chuckNorris().fact().replaceAll("Chuck Norris","Ali"));
+
+    }
+
+
+}
